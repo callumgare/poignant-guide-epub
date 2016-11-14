fdd63315d2257e4c7606057b43dda161d96f5377
Callum Gare <callum@gare.com.au>
2016-11-14 11:32:34 +1030

    Further optimisations for ebook layout
    Add a temporary book cover. This will be imporved in a future
    release. Fix broken content links.

--- make.rb
+++ make.rb
@@ -6,7 +6,15 @@ require 'css_parser'
 
 # Based on http://gist.github.com/424892. Thanks!
 
-DOC_TITLE = "why's (poignant) guide to ruby"
+DOC_TITLE = "why's (poignant) Guide to Ruby"
+
+# Thanks Phrogz! http://stackoverflow.com/a/4841269/847536
+class Nokogiri::XML::Node
+  def add_css_class( *classes )
+    existing = (self['class'] || "").split(/\s+/)
+    self['class'] = existing.concat(classes).uniq.join(" ")
+  end
+end
 
 def clean_page(src_dir, chapter_path)
   # make all pages xml complient
@@ -18,11 +26,30 @@ def clean_page(src_dir, chapter_path)
     doc.at_css('#sidepane').unlink
     doc.at_css('body').prepend_child( doc.at_css('#main') )
     doc.at_css('#main').prepend_child( doc.at_css('#wrapper') )
-    doc.css('script').unlink
-    doc.css("#bookcontents > ol > li a").each do |link|
-      link["href"] = "book/"+link["href"]
+    doc.at_css('.foxbox').unlink
+    doc.css("a").each do |link|
+      case link['href']
+      when /^chapter-\d+\.html/, /^expansion-pak-\d+\.html/
+        link["href"] = "book/"+link["href"]
+      when "../dwemthy/"
+        link["href"] = link["href"].sub(/\.\.\//,"")+"index.html"
+      end
+    end
+  when /^chapter-\d+\.html/, /^expansion-pak-\d+\.html/
+    doc.at_css('body').add_child( doc.at_css('.pageTitle') )
+    doc.at_css('body').add_child( doc.at_css('.content') )
+    doc.css('#banner').unlink
+    doc.css('#container').unlink
+    doc.css("p > img").each do |img|
+      img.parent.add_css_class('img');
+    end
+    doc.css("p > a").each do |link|
+      if link.content === "Turn page."
+        link.parent.unlink
+      end
     end
   end
+  doc.css('script').unlink
   File.write(File.join(src_dir, "book", chapter_path), doc.to_xhtml( encoding:'US-ASCII' ) )
 end
 
@@ -85,34 +112,55 @@ def get_styles(src_dir)
   parser.load_uri!(File.join(src_dir, 'guide.css'))
   # add a block of CSS
   css = <<-EOT
+    body, body.expansion {
+      initial;
+      padding: 0px
+    }
     .sidebar { 
       width: initial;
       float: initial;
       padding: initial;
-      margin: initial;
-      margin-left: initial;
       z-index: initial;
-      margin-right: 12px;
+      margin: 15px 0px;
     }
     .pageTitle {
       width: initial;
       max-width: 600px;
     }
-    #container {
-      background: initial;
-      width: initial;
+    * {
+      max-width: 100%;
     }
-    .pageLinks {
-      display: none;
+    p {
+      margin: 1em;
+      padding: 0;
     }
-    #banner {
-      display: none;
+    p.img {
+      margin: 0px;
+    }
+    p img, .content img {
+      display: block;
+      margin: auto;
+    }
+    pre,code {
+      word-wrap: break-word;      /* IE 5.5-7 */
+      white-space: -moz-pre-wrap; /* Firefox 1.0-2.0 */
+      white-space: pre-wrap;      /* current browsers */
+    }
+    .sidebar:after {
+      content: "End Sidebar!";
+    }
+    .content h2 {
+      margin: 35px 15px 15px;
+    }
+    .content h3 {
+      margin: 18px 15px 15px;
     }
   EOT
 
   parser.add_block!(css)
   File.write(File.join(src_dir, 'guide.css'), parser )
-  [{ File.join(src_dir, 'guide.css') => '.', :path => "guide.css", :dir => src_dir}]
+  [{ File.join(src_dir, 'guide.css') => '.', :path => "guide.css", :dir => src_dir},
+   { File.join(src_dir, 'dwemthy/dwemthy.css') => 'dwemthy'}]
 end
 
 def get_creators(src_dir)
@@ -176,11 +224,12 @@ if __FILE__ == $0
     title       DOC_TITLE
     creator     'why the lucky stiff'
     publisher   'why the lucky stiff'
+    cover       'cover.jpg'
     date        Time.now.strftime('%Y-%m-%d')
-    identifier  'http://mislav.uniqpath.com/poignant-guide/book/', :scheme => 'URL'
-    uid         'http://mislav.uniqpath.com/poignant-guide/book/'
+    identifier  'http://poignant.guide', :scheme => 'URL'
+    uid         'whys-poignant-guide-to-ruby'
 
-    files pages + images + styles
+    files pages + images + styles + [{ File.join(src_dir, '../cover.jpg') => '.' }]
     nav get_nav(pages)
   end
 
